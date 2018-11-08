a4c6c426f515ab1a73abfcf8ebfad7d9f54b34b1
malikshi <kidznightserver@gmail.com>
2018-11-09 00:14:29 +0700

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -11,8 +11,9 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/h1777/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 45244 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://github.com/malikshi/squid-proxy-installer/raw/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
     /sbin/iptables-save
     service squid restart
     update-rc.d squid defaults
@@ -22,8 +23,9 @@ elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04"; then
     touch /etc/squid3/passwd
     /bin/rm -f /etc/squid3/squid.conf
     /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/h1777/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 45244 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://github.com/malikshi/squid-proxy-installer/raw/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
     ln -s /etc/squid3 /etc/squid
@@ -38,7 +40,8 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie"; then
     /bin/rm -f /etc/squid3/squid.conf
     /usr/bin/touch /etc/squid3/blacklist.acl
     /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/h1777/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 45244 -j ACCEPT
+    /sbin/iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
     update-rc.d squid3 defaults
